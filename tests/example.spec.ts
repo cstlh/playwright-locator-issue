fd1b438cbea17e01d4ead733d570664a40e76bd8
cstlh <castilho.rs@gmail.com>
2025-01-27 17:46:53 +0100

    simplifying the html

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -11,16 +11,3 @@ test('check it is clicking the correct visible button', async ({ page }) => {
   await page.goto(fileUrl);
   await page.click('data-test-id=buttonTest >> visible="true"');
 });
-
-
-test.skip('Now check again but scroll a bit', async ({ page }) => {
-  //open the page.html file in playwright
-  const filePath = path.resolve(__dirname, '../page.html');
-  const fileUrl = `file://${filePath}`;
-  page.setDefaultTimeout(2000);
-  await page.goto(fileUrl);
-  await page.click('id=scrollHere');
-  await page.click('data-test-id=buttonTest >> visible="true"');
-});
-
-
